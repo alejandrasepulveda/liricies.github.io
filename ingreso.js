0b70f4c01504af2482564cc2ebf44180ea182956
aleja <fraisues@gmail.com>
2023-11-30 22:13:45 -0500

    js ingreso y registro

---
+++ ingreso.js
@@ -0,0 +1,19 @@
+function iniciarSesion() {
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    var usuarioGuardado = localStorage.getItem('usuario');
+    
+    if (usuarioGuardado) {
+        var usuario = JSON.parse(usuarioGuardado);
+
+        if (usuario.email === email && usuario.password === password) {
+            window.location.href = 'index.html';
+        } else {
+            alert('Correo electrónico o contraseña incorrectos. Inténtalo de nuevo.');
+        }
+    } else {
+        alert('No se encontaron usuarios registrados. Por favor regístrate primero.');
+    }
+}
--- registro.js
+++ registro.js
@@ -1,1 +1,20 @@
+function registrarUsuario() {
+
+    var nombres = document.getElementById('nombres').value;
+    var apellidos = document.getElementById('apellidos').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var genero = document.getElementById('genero').value;
+
+    var usuario = {
+        nombres: nombres,
+        apellidos: apellidos,
+        email: email,
+        genero: genero
+    };
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    window.location.href = 'ingreso.html';
+}
 
