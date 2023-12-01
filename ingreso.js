ae54aa5f35be8411dbfc6ba53daf6bdf24fdccbc
aleja <fraisues@gmail.com>
2023-11-30 23:15:36 -0500

    arreglo problema con la contraseña

--- ingreso.js
+++ ingreso.js
@@ -1,19 +1,22 @@
 function iniciarSesion() {
-
+    // Obtener valores del formulario
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
+    // Obtener usuario del localStorage (simulado)
     var usuarioGuardado = localStorage.getItem('usuario');
     
     if (usuarioGuardado) {
         var usuario = JSON.parse(usuarioGuardado);
 
+    
         if (usuario.email === email && usuario.password === password) {
+
             window.location.href = 'index.html';
         } else {
             alert('Correo electrónico o contraseña incorrectos. Inténtalo de nuevo.');
         }
     } else {
-        alert('No se encontaron usuarios registrados. Por favor regístrate primero.');
+        alert('No hay usuarios registrados. Regístrate primero.');
     }
 }
--- registro.js
+++ registro.js
@@ -1,20 +1,26 @@
 function registrarUsuario() {
-
-    var nombres = document.getElementById('nombres').value;
-    var apellidos = document.getElementById('apellidos').value;
+    // Obtener valores del formulario
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var genero = document.getElementById('genero').value;
 
+    // Crear objeto de usuario
     var usuario = {
-        nombres: nombres,
-        apellidos: apellidos,
+        nombre: nombre,
+        apellido: apellido,
         email: email,
+        password: password,  // No recomendado en un entorno real. En producción, deberías hashear la contraseña.
         genero: genero
     };
 
+    // Guardar usuario en el localStorage
     localStorage.setItem('usuario', JSON.stringify(usuario));
 
+    // Redirigir a la página de inicio de sesión (simulado)
     window.location.href = 'ingreso.html';
 }
+
+
 
