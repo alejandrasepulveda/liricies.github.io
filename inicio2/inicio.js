6a318c461337ed9723cf388b676817b9f58e3512
aleja <fraisues@gmail.com>
2023-12-01 10:59:34 -0500

    detalles finales, arreglos

--- inicio2/inicio.js
+++ inicio2/inicio.js
@@ -12,3 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.querySelector('.navbar__text');
+
+    if (menuIcon && navList) {
+        menuIcon.addEventListener('click', function () {
+            navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+        });
+    }
+});
